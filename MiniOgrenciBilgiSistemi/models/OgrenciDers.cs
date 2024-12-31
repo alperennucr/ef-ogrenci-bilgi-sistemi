e9fd9f029d6922ed126af298fe7f0b9256580832
alperen <ucaralperen003@gmail.com>
2024-12-31 08:19:34 +0300

    first commit, ef structure without form side

---
+++ MiniOgrenciBilgiSistemi/models/OgrenciDers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniOgrenciBilgiSistemi.models
+{
+    internal class OgrenciDers
+    {
+        public int DersId { get; set; }
+        public int OgrenciId { get; set; }
+
+
+        
+        public Ogrenci Ogrenci { get; set; }
+        public Ders Ders { get; set; }
+        
+    }
+}
---
+++ MiniOgrenciBilgiSistemi/models/Sinif.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniOgrenciBilgiSistemi.models
+{
+    internal class Sinif
+    {
+        public int SinifId { get; set; }
+        public string SinifAd { get; set; }
+        public byte Kontenjan { get; set; }
+ 
+        public ICollection<Ogrenci> Ogrenciler { get; set; }
+    }
+}
